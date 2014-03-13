3fa9361a2a535e3eaf666a5d4c9ac91b2188e4c9
rhonan <rhonanc@gmail.com>
2014-03-13 13:42:05 -0300

    Simulador de envio de mensagens para os navegadores.

--- src/main/java/br/ufc/quixada/websocket/WSocket.java
+++ src/main/java/br/ufc/quixada/websocket/WSocket.java
@@ -1,9 +1,12 @@
 package br.ufc.quixada.websocket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -30,10 +33,32 @@ public class WSocket {
         Observacao observacao = new Observacao(-14.239424,-53.186502, 180,"TESTE", "ADD");
         Gson gson = new Gson();
         peer.getBasicRemote().sendText(gson.toJson(observacao));
+        simulador();
     }
 
     @OnClose
     public void onClose (Session peer) {
         peers.remove(peer);
     }
+    
+    //Formato da mensagem: latitude,longitude,grau,hex,status
+    //Exemplo: -14.239424,-53.186502,180,TESTE,ADD
+    private static void simulador() throws IOException{
+    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));  
+    	Gson gson = new Gson();
+    	String mensagem;
+    	
+    	while(true){
+    		System.out.println(":");
+    		
+    		mensagem = in.readLine();
+			String[] mensagem_quebrada = mensagem.split(Pattern.quote(","));
+    		
+        	for(Session peer : peers){
+                peer.getBasicRemote().sendText(gson.toJson(new Observacao(Double.parseDouble(mensagem_quebrada[0]),Double.parseDouble(mensagem_quebrada[1]), Integer.parseInt(mensagem_quebrada[2]),mensagem_quebrada[3], mensagem_quebrada[4])));
+        	}
+    		
+    	}
+    }
+    
 }
